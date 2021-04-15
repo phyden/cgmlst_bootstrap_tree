ba3380eb529137891eb7d93ea7bb15c67f2e55d1
phyden <patrick_hyden@gmx.at>
2021-04-15 09:51:00 +0200

    FEAT: adding argument parser

--- bootstrap_alleles.py
+++ bootstrap_alleles.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import argparse
 import numpy as np
 import pandas as pd
 from Bio.Phylo.TreeConstruction import DistanceTreeConstructor, DistanceMatrix
@@ -8,9 +9,22 @@ from Bio import Phylo
 from Bio.Phylo.Consensus import get_support
 import matplotlib.pyplot as plt
 
-# input table:
-# should be col 1: sample id, rest integer (allele numbers)
-table="./Human_oesterreich-2021.csv"
+parser = argparse.ArgumentParser(description="Routine to create bootstrap trees from categorical data like"
+                                             "core genome MLST")
+parser.add_argument("-i", "--input_table", dest="table",
+                    help="Input table as .csv e.g. exported from SeqSphere")
+parser.add_argument("-o", "--output", dest="output", default="tree.nwk",
+                    help="destination to write newick tree [./tree.nwk]")
+parser.add_argument("-t", "--times", dest="times", type=int, default=100,
+                    help="Number of bootstrap samples taken [100]")
+parser.add_argument("--tree", dest="tree", default=None,
+                    help="Newick tree file to add support values, if not provided a new one is created")
+parser.add_argument("--upgma", action="store_true", default=False,
+                    help="create UPGMA instead of NJ trees")
+parser.add_argument("-d", "--delim", dest="delim", type=str, default=";",
+                    help="Field delimiter [;]")
+
+
 
 def get_distance_matrix(array, names):
     d2 = array[:, np.newaxis,:]
@@ -33,24 +47,42 @@ def bootstrap(array, times):
         yield array[:, np.random.choice(array.shape[1], int(array.shape[1] // ( 1 / 0.9 )), replace=False)]
 
 
-df = pd.read_csv(table, sep=';', quotechar='"', index_col=0)
-for c in df.columns:
-    df[c] = pd.to_numeric(df[c], errors="coerce", downcast="integer")
+def main():
+    args = parser.parse_args()
 
-data = df.values.astype(int)
-names = df.index.tolist()
+    table = args.table
+    if not table:
+        print("No input table provided. This argument is required\n")
+        parser.print_help()
+        exit(0)
+    df = pd.read_csv(table, sep=args.delim, quotechar='"', index_col=0)
+    for c in df.columns:
+        if c.lower() == "sample id":
+            df.set_index(c)
+        else:
+            df[c] = pd.to_numeric(df[c], errors="coerce", downcast="integer")
 
-constructor = DistanceTreeConstructor()
-main_tree = constructor.nj(m)
+    data = df.values.astype(int)
+    names = df.index.tolist()
 
-main_tree = constructor.nj(get_distance_matrix(data, names))
+    constructor = DistanceTreeConstructor()
+    if args.umgpa:
+        const = constructor.upgma
+    else:
+        const = constructor.nj
+
+    if args.tree:
+        main_tree = Phylo.read(args.tree, "newick")
+    else:
+        main_tree = const(get_distance_matrix(data, names))
 
 
-trees = [constructor.nj(get_distance_matrix(sub, names)) for sub in bootstrap(data, 100)]
+    trees = [const(get_distance_matrix(sub, names)) for sub in bootstrap(data, args.times)]
 
-supp_tree = get_support(main_tree, trees)
+    supp_tree = get_support(main_tree, trees)
 
-Phylo.draw(supp_tree)
-plt.show()
 
-Phylo.write(supp_tree, "mytree.nwk", "newick")
+    Phylo.write(supp_tree, args.output, "newick")
+
+if __name__ == '__main__':
+    main()
