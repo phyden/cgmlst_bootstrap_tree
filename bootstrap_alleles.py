7cbbed9bf31a0c23ea2dd36d51208b5c117436c5
phyden <patrick_hyden@gmx.at>
2021-04-15 10:27:40 +0200

    MAINT: minor refactoring

--- bootstrap_alleles.py
+++ bootstrap_alleles.py
@@ -7,9 +7,10 @@ import pandas as pd
 from Bio.Phylo.TreeConstruction import DistanceTreeConstructor, DistanceMatrix
 from Bio import Phylo
 from Bio.Phylo.Consensus import get_support
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="Routine to create bootstrap trees from categorical data like"
-                                             "core genome MLST")
+                                             " core genome MLST")
 parser.add_argument("-i", "--input_table", dest="table",
                     help="Input table as .csv e.g. exported from SeqSphere")
 parser.add_argument("-o", "--output", dest="output", default="tree.nwk",
@@ -24,10 +25,9 @@ parser.add_argument("-d", "--delim", dest="delim", type=str, default=";",
                     help="Field delimiter [;]")
 
 
-
 def get_distance_matrix(array, names):
-    d2 = array[:, np.newaxis,:]
-    d2_t = d2.reshape((d2.shape[1],d2.shape[0],d2.shape[2]))
+    d2 = array[:, np.newaxis, :]
+    d2_t = d2.reshape((d2.shape[1], d2.shape[0], d2.shape[2]))
     dist = 1 - np.isclose(d2, d2_t)
     return to_bio_matrix(np.mean(dist, axis=2), names)
 
@@ -35,15 +35,15 @@ def get_distance_matrix(array, names):
 def to_bio_matrix(array, names):
     matrix = []
     for i in range(0, array.shape[0]):
-        matrix.append(array[i,:].tolist()[:i+1])
+        matrix.append(array[i, :].tolist()[:i + 1])
     return DistanceMatrix(names, matrix)
-    
+
 
 def bootstrap(array, times):
     i = 0
     while i < times:
         i += 1
-        yield array[:, np.random.choice(array.shape[1], int(array.shape[1] // ( 1 / 0.9 )), replace=False)]
+        yield array[:, np.random.choice(array.shape[1], int(array.shape[1] * 0.9), replace=False)]
 
 
 def main():
@@ -54,6 +54,7 @@ def main():
         print("No input table provided. This argument is required\n")
         parser.print_help()
         exit(0)
+
     df = pd.read_csv(table, sep=args.delim, quotechar='"', index_col=0)
     for c in df.columns:
         if c.lower() == "sample id":
@@ -75,11 +76,8 @@ def main():
     else:
         main_tree = const(get_distance_matrix(data, names))
 
-
     trees = [const(get_distance_matrix(sub, names)) for sub in bootstrap(data, args.times)]
-
     supp_tree = get_support(main_tree, trees)
-
 
     # remove labels of inner clades
     for clade in supp_tree.get_nonterminals():
